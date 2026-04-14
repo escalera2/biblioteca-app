a47f5dfbd2ec6d45b338495e77994ca3e19ef58d
fmoises10 <felixcastellon10@gmail.com>
2026-04-13 20:05:00 -0400

    modulo prestamos agregado

---
+++ src/app/pages/prestamos/prestamos-form/prestamos-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamos-form',
+  imports: [],
+  templateUrl: './prestamos-form.html',
+  styleUrl: './prestamos-form.scss',
+})
+export class PrestamosForm {
+
+}
